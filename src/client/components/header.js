40d08a4f281b791327d03f72e02759d3e06bceb1
Schyler Hosler <schylerhosler@gmail.com>
2019-04-14 13:55:58 -0400

    Add initial font related variables and refactor Header to more closely match design spec

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -9,12 +9,16 @@ const Header = () => {
    * Base CSS class
    * @returns {ReactElement}
    */
-  const baseCls = 'header';
+  const baseCls = 'header',
+        containerCls = `${baseCls}__container container`,
+        linkCls = `${baseCls}__link`
 
   return (
-    <div className={baseCls}>
-      <Link to="/">MyTodos</Link>
-    </div>
+    <header className={baseCls}>
+      <div className={containerCls}>
+        <Link className={linkCls} to="/">MyTodos</Link>
+      </div>
+    </header>
   )
 };
 
